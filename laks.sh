d0cf70079b235431e90e017a85094356def57b1d
Jantunganton <97970942+Jantunganton@users.noreply.github.com>
2022-02-19 20:01:44 +0700

    Update laks.sh

--- laks.sh
+++ laks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 POOL=https://server1.whalestonpool.com
-WALLET=0x05Db5979Ce5b0D7a1C1bE1753983e8C793a86FA5
+WALLET=EQCcN3oflV6VAFtZVD4yZ8xOypZ05NWQPK1VKY7hPDLsIxzb
 
 chmod +x ./gilagilagila && ./gilagilagila run $POOL $WALLET
